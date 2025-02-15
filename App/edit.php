8dfe9f13af6e88e725ded1b2493e22d0236fb483
kevinmorelo13 <kmorelo89@gmail.com>
2025-02-15 09:11:08 -0500

    Second Commit

--- App/edit.php
+++ App/edit.php
@@ -33,10 +33,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Producto</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
 <style>
     body {
-        font-family: system-ui;
+        font-family: 'Poppins', sans-serif;
         margin: 20px;
         display: flex;
         flex-direction: column;
@@ -49,8 +53,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     input {
         padding: 10px;
         border: 2px solid #ccc;
-        border-radius: 5px;
-        font-size: 16px;
+        border-radius: 10px;
+        font-size: 14px;
         color: #555;
         outline: none;
 
@@ -58,21 +62,67 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     button {
         padding: 10px;
-        border: 2px solid #ccc;
+        border: none;
         border-radius: 5px;
         font-size: 12px;
-        color: #555;
+        color: white;
         outline: none;
+        font-weight: 600;
+        background-color: #363636;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #999999;
+        color: black;
+    }
+
+    .nombre {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .precio {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        gap: 10px;
+
+    }
+
+
+    label {
+        font-size: 12px;
+        font-weight: 600;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+        font-weight: 600;
     }
 </style>
 
 <body>
     <h1>Editar Producto</h1>
     <form method="post">
-        <label>Nombre:</label>
-        <input type="text" name="nombreProducto" value="<?= htmlspecialchars($producto['nombreProducto']) ?>" required>
-        <label>Precio:</label>
-        <input type="number" name="precio" value="<?= htmlspecialchars($producto['precio']) ?>" step="0.01" required>
+        <div class="nombre">
+            <label>Nombre:</label>
+            <input type="text" name="nombreProducto" value="<?= htmlspecialchars($producto['nombreProducto']) ?>" required>
+        </div>
+        <div class="precio">
+            <label>Precio:</label>
+            <input type="number" name="precio" value="<?= htmlspecialchars($producto['precio']) ?>" step="0.01" required>
+        </div>
         <button type="submit">Guardar Cambios</button>
     </form>
     <br>
--- App/index.php
+++ App/index.php
@@ -21,9 +21,12 @@ $productos = $controller->listarProductos();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestión de Productos</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <style>
         body {
-            font-family: system-ui;
+            font-family: "Poppins", sans-serif;
             margin: 20px;
             display: flex;
             flex-direction: column;
@@ -36,12 +39,15 @@ $productos = $controller->listarProductos();
         table {
             width: 50%;
             border-collapse: collapse;
+
         }
 
         th,
         td {
+            background-color: #fff;
             padding: 4px;
             border: 1px solid #ddd;
+            text-align: center;
         }
 
         th {
@@ -49,47 +55,42 @@ $productos = $controller->listarProductos();
         }
 
         input {
-            padding: 10px;
-            border: 2px solid #ccc;
+            padding: 8px;
             border-radius: 5px;
-            font-size: 16px;
-            color: #555;
-            outline: none;
-
+            border: 2px solid #ddd;
+            color: black;
         }
 
         button {
-            padding: 10px;
-            border: 2px solid #ccc;
+            padding: 6px;
+            border: none;
             border-radius: 5px;
             font-size: 12px;
-            color: black;
+
             outline: none;
             font-weight: 600;
+            background-color: #dfdfdf;
+            color: #363636;
+            cursor: pointer;
         }
 
-        a {
-            text-decoration: none;
-            color: black;
-            font-size: 12px;
-            outline: none;
-            border: 2px solid #ccc;
-            border-radius: 5px;
-            padding: 10px 5px;
-            background-color: #F0F0F0;
-            font-weight: 600;
+        button:hover {
+            background-color: #c8c8c8;
+            color: #363636;
+
         }
     </style>
 </head>
 
 <body>
-    <h1>Gestión de Productos</h1>
+    <h1 class="titulo">Gestión de Productos</h1>
 
     <h2>Agregar Nuevo Producto</h2>
     <form method="post">
         <input type="text" name="nombreProducto" placeholder="Nombre del producto" required>
         <input type="number" name="precio" placeholder="Precio" step="0.01" required>
-        <button type="submit" name="agregar">Agregar Producto</button>
+        <button type="submit" id="agregar" name="agregar">Agregar Producto</button>
+
     </form>
 
     <h2>Lista de Productos</h2>
@@ -99,8 +100,8 @@ $productos = $controller->listarProductos();
                 <th>ID</th>
                 <th>Nombre</th>
                 <th>Precio</th>
-                <th>Editar</th>
-                <th>Eliminar</th>
+                <th>Acciones</th>
+
             </tr>
         </thead>
         <tbody>
@@ -112,12 +113,30 @@ $productos = $controller->listarProductos();
                     <td>
                         <form method="post" style="display: inline;">
                             <input type="hidden" name="id" value="<?= $producto['id'] ?>">
-                            <button type="submit" name="eliminar">Eliminar</button>
+                            <button class="eliminar" type="submit" name="eliminar" style="background-color: #fff0f0;">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ff5757" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M4 7l16 0" />
+                                    <path d="M10 11l0 6" />
+                                    <path d="M14 11l0 6" />
+                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                </svg>
+                            </button>
                         </form>
-                    <td>
+
                         <form method="post" style="display: inline;">
                             <input type="hidden" name="id" value="<?= $producto['id'] ?>">
-                            <a href="edit.php?id=<?= $producto['id'] ?>">Editar</a>
+                            <a href="edit.php?id=<?= $producto['id'] ?>">
+                                <button type="button" style="background-color: #fdffe7;">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff80d" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                                        <path d="M13.5 6.5l4 4" />
+                                    </svg>
+                                </button>
+                            </a>
+
                         </form>
                     </td>
 
